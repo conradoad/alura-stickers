69d02d911647f0cd79dc5f56e9b47d93765039ae
Conrado Costa <conradocostaad@yahoo.com.br>
2022-07-24 15:13:38 -0300

    Reorganize in order to maintain the api project in a different repository

--- languages-api/src/main/java/br/com/alura/languages/api/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.languages.api;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LanguageRepository extends MongoRepository<Language, String>{
-    
-}
